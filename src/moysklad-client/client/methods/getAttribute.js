f4d79dd87c0c61c2dcf01a9b280904c7b28ce5e1
Makeev Vitaliy <w.makeev@gmail.com>
2014-04-20 14:06:39 +0600

    [идет разрабаботка]

---
+++ src/moysklad-client/client/methods/getAttribute.js
@@ -0,0 +1,25 @@
+/**
+ * getAttribute
+ * Date: 20.04.14
+ * Vitaliy V. Makeev (w.makeev@gmail.com)
+ */
+
+var _ = require('lodash')
+    , callbackAdapter = require('../../../tools/index').callbackAdapter;
+
+
+var stock = function () {
+    var args        = _.toArray(arguments)
+        , callback    = typeof args.slice(-1)[0] === 'function' ? args.slice(-1)[0] : null
+        , options     = typeof args[0] === 'object' ? args[0] : {}
+        , _restClient = this.getProvider('stock-json')
+        , _obj        = null;
+
+    _restClient.stock(options, function (err, data) {
+        _obj = callbackAdapter(err, data.obj, callback);
+    });
+
+    return _obj;
+};
+
+module.exports = stock;
