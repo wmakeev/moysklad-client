5db39e8f2254ce1bc9cfb21bb96ca65d8c079a2f
Vitaliy Makeev <w.makeev@gmail.com>
2015-02-10 11:00:46 +0500

    add `client.del` method

---
+++ src/moysklad-client/client/methods/del.js
@@ -0,0 +1,39 @@
+/**
+ * del
+ * Date: 10.02.15
+ * Vitaliy V. Makeev (w.makeev@gmail.com)
+ */
+
+var _               = require('lodash')
+  , callbackAdapter = require('../../../tools/index').callbackAdapter;
+
+//TODO Ограничение на кол-во сохраняемых объектов в коллекции (проверить)
+
+/**
+ * Del. Удаляет сущность или список сущностей.
+ *
+ * @param {String} [type] Тип сущности (если не указан производится попытка получить тип из свойства объекта TYPE_NAME)
+ * @param {Object} ent Сущность или список сущностей
+ * @param {Function=} callback
+ * @returns {Object} Созданная/сохраненная сущность
+ */
+var del = function () {
+    //TODO Ensure
+    var args        = _.toArray(arguments)
+      , callback    = typeof args.slice(-1)[0] === 'function' ? args.slice(-1)[0] : null;
+
+    var restClient  = this.getProvider('ms-xml'),
+        obj = null;
+
+    var putArgs = args.slice(0, args.length);
+
+    putArgs.push(function (err, data) {
+        obj = callbackAdapter(err, data.obj, callback);
+    });
+
+    restClient.del.apply(restClient, putArgs);
+
+    return obj;
+};
+
+module.exports = del;
