6bcdb0a72d4a3d117e7cd4a097dc0e19cd3a5dcc
Vitaliy Makeev <w.makeev@gmail.com>
2015-06-01 17:40:07 +0500

    Рефракторинг
    Централизованное указание основных провайдеров при создании клиента. Более удобное тестирование #15

--- src/moysklad-client/index.js
+++ src/moysklad-client/index.js
@@ -4,22 +4,60 @@
  * Vitaliy V. Makeev (w.makeev@gmail.com)
  */
 
-var client = require('./client')
-  , query  = require('./rest-clients/ms-xml/query')
-  , logger = require('project/logger')
-  , pkg    = require('../../package');
+var _             = require('lodash'),
+    have          = require('have'),
+    pkg           = require('../../package'),
+    client        = require('./client'),
+    logger        = require('project/logger'),
+    tools         = require('project/tools'),
+    fetchProvider = require('project/fetch'),
+    query         = require('./rest-clients/ms-xml/query'),
+    xmlClient     = require('./rest-clients/ms-xml'),
+    jsonServices  = require('./rest-clients/json');
+
 
 logger.info('moysklad-client v' + pkg.version);
 
 module.exports = {
 
     createClient: function () {
-        return client.apply(this, [null].concat(Array.prototype.slice.call(arguments, 0)));
+        var args = have(arguments, {
+            login   : 'optional string',
+            password: 'optional string',
+            options : 'optional object'
+        });
+
+        var clientInstance = client();
+
+        if (args.options)
+            _.extend(clientInstance.options, args.options);
+
+        if (args.login && args.password)
+            clientInstance.setAuth(args.login, args.password);
+
+        var providers = {
+            'fetch'         : fetchProvider,
+            'marshaller'    : clientInstance.context.createMarshaller(),
+            'unmarshaller'  : clientInstance.context.createUnmarshaller(),
+            'logger'        : logger
+        };
+
+        xmlClientInstance = xmlClient().setProvider(providers);
+        _.extend(xmlClientInstance.options, clientInstance.options);
+
+        jsonServicesInstance = jsonServices().setProvider(providers);
+        _.extend(jsonServicesInstance.options, clientInstance.options);
+
+        clientInstance
+            .setProvider('ms-xml', xmlClientInstance)
+            .setProvider('json-services', jsonServicesInstance);
+
+        return clientInstance;
     },
 
     createQuery: query.createQuery,
 
-    tools: require('project/tools'),
-    logger: require('project/logger'),
+    tools: tools,
+    logger: logger,
     version: pkg.version
 };
