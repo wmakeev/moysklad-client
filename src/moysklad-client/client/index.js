e65ffa9b8401420740bd4de1d7c1ae6f5092b102
Vitaliy Makeev <w.makeev@gmail.com>
2015-12-02 10:26:55 +0500

    Вынесен во внешний интерфейс метод удаления сущностей `client.del()`

--- src/moysklad-client/client/index.js
+++ src/moysklad-client/client/index.js
@@ -4,30 +4,30 @@
  * Vitaliy V. Makeev (w.makeev@gmail.com)
  */
 
-var _                         = require('lodash')
-  , stampit                   = require('stampit')
-  , Query                     = require('./../rest-clients/ms-xml/query')
-  , operators                 = require('./../rest-clients/ms-xml/query/operators')
-  , authProviderBehavior      = require('./../../authProviderBehavior')
-  , providerAccessorBehavior  = require('./../../providerAccessorBehavior');
+var _                        = require('lodash'),
+    stampit                  = require('stampit'),
+    Query                    = require('./../rest-clients/ms-xml/query'),
+    operators                = require('./../rest-clients/ms-xml/query/operators'),
+    authProviderBehavior     = require('./../../authProviderBehavior'),
+    providerAccessorBehavior = require('./../../providerAccessorBehavior');
 
 /**
  * @lends Client.prototype
  */
 var clientMethods = {
     // Ms
-    from:   require('./methods/from'),
-    load:   require('./methods/load'),
-    chain:  require('./methods/chain'),
-    first:  require('./methods/first'),
-    total:  require('./methods/total'),
-    save:   require('./methods/save'),
-
+    from: require('./methods/from'),
+    load: require('./methods/load'),
+    chain: require('./methods/chain'),
+    first: require('./methods/first'),
+    total: require('./methods/total'),
+    save: require('./methods/save'),
+    del: require('./methods/del'),
     // Query
     createQuery: Query.createQuery,
 
     // LazyLoader
-    createLazyLoader:   require('./lazy-loader')
+    createLazyLoader: require('./lazy-loader')
 };
 
 var jsonServiceMethods = require('./methods/json-service');
@@ -37,7 +37,7 @@ var jsonServiceMethods = require('./methods/json-service');
  */
 var Client = stampit()
 
-    // Options
+// Options
     .state({
         options: {
             filterLimit: 50,
