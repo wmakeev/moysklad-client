6df4af5cde91684f972c41d11cdd7f5b23b6ca2b
Makeev Vitaliy <w.makeev@gmail.com>
2016-04-24 14:36:00 +0500

    Рефакторинг
    client.del

--- src/moysklad-client/client/methods/del.js
+++ src/moysklad-client/client/methods/del.js
@@ -1,32 +1,31 @@
-var _ = require('lodash')
 var have = require('project/have')
-var CallbackAdapter = require('project/callback-adapter');
+var CallbackAdapter = require('project/callback-adapter')
 
-//TODO Ограничение на кол-во удаляемых объектов в коллекции (проверить)
+// TODO Ограничение на кол-во удаляемых объектов в коллекции (проверить)
 
 /**
- * Del
- * Удаляет объекты
+ * Delete. Удаление объектов.
+ *
+ * @return {Array.<string>} Список идентификаторов удаленных сущностей
  */
-function del() {
-    var args = have(arguments, [
-        { entity: 'Entity', callback: 'opt function' },
-        { entities: 'Entity array', callback: 'opt function' },
-        { type: 'string', uuid: 'UUID', callback: 'opt function' },
-        { type: 'string', uuids: 'UUID arr', callback: 'opt function' }
-    ])
+function del () {
+  var args = have(arguments, [
+    { entity: 'Entity', callback: 'opt function' },
+    { entities: 'Entity array', callback: 'opt function' },
+    { type: 'string', uuid: 'UUID', callback: 'opt function' },
+    { type: 'string', uuids: 'UUID arr', callback: 'opt function' }
+  ])
 
-    var that = this
-    var cb = args.callback
-    var callbackAdapter = new CallbackAdapter(that.options.flowControl, cb)
+  var cb = args.callback
+  var callbackAdapter = new CallbackAdapter(this.options.flowControl, cb)
 
-    var restClient = this.getProvider('ms-xml')
+  var restClient = this.getProvider('ms-xml')
 
-    delete args.callback
-    restClient.del(args, function (err, data) {
-        callbackAdapter.done(err, /* TODO !!! */ data.obj)
-    })
+  delete args.callback
+  restClient.del(args, function (err, data) {
+    callbackAdapter.done(err, /* TODO !!! */ data.obj)
+  })
 
-    return callbackAdapter.resolve();
+  return callbackAdapter.resolve()
 }
-module.exports = del;
+module.exports = del
