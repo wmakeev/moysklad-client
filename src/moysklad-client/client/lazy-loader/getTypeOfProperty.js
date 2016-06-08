0cbeb16624efff0c4df189e08192bc7457008121
Makeev Vitaliy <w.makeev@gmail.com>
2016-06-08 17:12:12 +0500

    Исправление в LazyLoader'е
    Исправление бага, который приводил к ошибке, в частности, при обращении к свойству `contract.agent` (и в других возможных случаях).

--- src/moysklad-client/client/lazy-loader/getTypeOfProperty.js
+++ src/moysklad-client/client/lazy-loader/getTypeOfProperty.js
@@ -9,14 +9,15 @@ var propMap = require('./nameToTypeMap');
 
 function getTypeOfProperty(propertyName, entity) {
 
-    if (entity.TYPE_NAME && propMap[entity.TYPE_NAME] && propMap[entity.TYPE_NAME][propertyName])
+    if (entity.TYPE_NAME && propMap[entity.TYPE_NAME] && propMap[entity.TYPE_NAME][propertyName]) {
         return propMap[entity.TYPE_NAME][propertyName];
+    }
 
-    else if (propMap[propertyName])
+    if (propMap[propertyName]) {
         return propMap[propertyName];
+    }
 
-    else
-        return propertyName;
+    return propertyName;
 }
 
 module.exports = getTypeOfProperty;
